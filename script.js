d83e4e855c8fd2d6199b4cd84809983a2138cac6
karthikeka <119919351+Karthikeka@users.noreply.github.com>
2023-04-24 14:16:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var data=document.querySelector('h2');
+
+setTimeout(()=>{
+    data.innerText='10'
+    setTimeout(()=>{
+        data.innerText='9'
+        setTimeout(()=>{
+            data.innerText='8'
+            setTimeout(()=>{
+                data.innerText='7'
+                setTimeout(()=>{
+                    data.innerText='6'
+                    setTimeout(()=>{
+                        data.innerText='5'
+                        setTimeout(()=>{
+                            data.innerText='4'
+                            setTimeout(()=>{
+                                data.innerText='3'
+                                setTimeout(()=>{
+                                    data.innerText='2'
+                                    setTimeout(()=>{
+                                        data.innerText='1'
+                                        setTimeout(()=>{
+                                            data.innerText='HAPPY TAMIL NEW YEAR'
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
